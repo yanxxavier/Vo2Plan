296906cff0f2de32b54ad4a80e479b0c29257bdf
yanxxavier <yanxavierlindolfo@gmail.com>
2024-05-02 15:31:10 -0300

    add navBar and section1/section2

---
+++ assets/js/vo2.js
@@ -0,0 +1,10 @@
+function toggleMenu () {
+   const button = document.querySelector('.js-menu');
+   const links = document.querySelector('.nav-links');
+   const linkState = links.classList;
+   
+    linkState.toggle('open')
+   
+   
+   
+}
